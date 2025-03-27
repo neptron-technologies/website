fe90db4cdcdf9feebe4c0c3dcf99d9f1fdc4be4d
SurajSingh-Neptron <suraj.s@neptrontech.com>
2025-03-27 17:07:35 +0530

    Neptron-Website Template

---
+++ Nep-Website.Server/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Nep_Website.Server
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
